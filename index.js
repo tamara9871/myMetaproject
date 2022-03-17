54c562589b76a36c185bc1f0875b128cc121b411
Metamara <83432760+tamara9871@users.noreply.github.com>
2022-03-17 16:32:56 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const ratio = window.innerWidth / window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, ratio, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+renderer.render( scene, camera);
